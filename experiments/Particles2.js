c6a31c4295359334c21193a82f9b3e65355be8b7
markramlich <krme23gb@student.ju.se>
2025-09-24 00:32:09 +0700

    A new particles artwork

---
+++ experiments/Particles2.js
@@ -0,0 +1,87 @@
+let startflower = 0;
+let particles = [];
+let lines = [];
+let flowerPositions = [];
+let nextIndex = 0;
+let startTime = 0;
+
+class Particle {
+  constructor(x, y) {
+    this.position = createVector(x, y);
+
+    const a = Math.random() * Math.PI * 2;
+    const v = 0.2 + Math.random();
+    this.velocity = createVector(Math.cos(a) * v, Math.sin(a) * v);
+    this.lifespan = 100 + Math.random() * 100;
+
+    this.color = color(random(200, 255), random(100, 160), random(0, 50), 255);
+  }
+
+  update() {
+    this.lifespan--;
+
+    this.velocity.mult(0.99);
+    this.position.add(this.velocity);
+  }
+
+  draw() {
+    push();
+    translate(this.position.x, this.position.y);
+    noStroke();
+
+    fill(red(this.color), green(this.color), blue(this.color), 1);
+    ellipse(0, 0, 6);
+    pop();
+  }
+
+  isDead() {
+    return this.lifespan <= 0;
+  }
+}
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  blendMode(ADD);
+  let xPos = 50;
+
+  while (xPos < innerWidth) {
+    flowerPositions.push({ x: xPos, y: random(100, 500) });
+    xPos += random(20, 120);
+  }
+  startTime = millis();
+}
+
+function generateParticles(x, y) {
+  for (let i = 0; i < 400; i++) {
+    const px = x + random(-10, 10);
+    const py = y + random(-10, 10);
+    const particle = new Particle(px, py);
+    particles.push(particle);
+  }
+}
+
+function draw() {
+  background(0, 0, 0);
+  if (
+    nextIndex < flowerPositions.length &&
+    millis() - startTime > nextIndex * 1000
+  ) {
+    let pos = flowerPositions[nextIndex];
+    generateParticles(pos.x, pos.y);
+    lines.push({ x: pos.x, y: pos.y });
+    nextIndex++;
+  }
+  for (let particle of particles) {
+    particle.update();
+    particle.draw();
+
+    if (particle.isDead()) {
+      particles.splice(particles.indexOf(particle), 1);
+    }
+  }
+  stroke(0.8);
+  strokeWeight(0.5);
+  for (let linePos of lines) {
+    line(linePos.x, linePos.y, linePos.x, height);
+  }
+}
